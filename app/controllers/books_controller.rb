bf443a56f7666f49885d8b2dd46d6a205fd26e84
martinbicycle <martinbicycle7700@gmail.com>
2020-12-04 10:59:34 +0000

    [Add] 3-8 投稿機能未完成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,20 @@
 class BooksController < ApplicationController
   def new
-    @books = Book.all
+    @book = Book.new
+  end
+  
+   def create
+    # １. データを新規登録するためのインスタンス作成
+    book = Book.new(book_params)
+    # ２. データをデータベースに保存するためのsaveメソッド実行
+    book.save
+    # ３. トップ画面へリダイレクト
+    redirect_to '/tops'
+   end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
