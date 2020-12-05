32dd7c69fa0f0fbe298bd25fb84fa1609613012c
martinbicycle <martinbicycle7700@gmail.com>
2020-12-05 14:43:10 +0000

    [Add] 3-8 削除機能の追加

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,5 @@ Rails.application.routes.draw do
   get 'books/:id/edit' => 'books#edit', as: 'edit_book'
   patch 'books/:id' => 'books#update', as: 'update_book'
   put 'books/:id' => 'books#update'
-  delete 'books/:id' => 'books#destroy'
+  delete 'books/:id' => 'books#destroy', as: 'destroy_book'
 end
