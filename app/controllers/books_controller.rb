8febb5dc40f8b360f175f2ccc5d3f2ac5723be95
martinbicycle <martinbicycle7700@gmail.com>
2020-12-05 10:03:06 +0000

    [Add] 3-8 編集機能、詳細機能作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,25 +1,45 @@
 class BooksController < ApplicationController
+
   def new
     @book = Book.new
   end
 
-   def create
-    # １. データを新規登録するためのインスタンス作成
-    book = Book.new(book_params)
-    # ２. データをデータベースに保存するためのsaveメソッド実行
-    book.save
+  def create
+      @book = Book.new(book_params)
+      if @book.save
+         redirect_to book_path(@book)
+      else
+         render :new
+      end
+  end
 
-    redirect_to '/books'
-   end
+  def index
+      @books = Book.all
+      @book = Book.new
+  end
 
-   def index
-     @books = Book.all
-     @book = Book.new
-   end
+  def show
+      @book = Book.find(params[:id])
+  end
+
+  def edit
+      @book = Book.find(params[:id])
+  end
+
+  def update
+      @book = Book.find(params[:id])
+      @book.update(book_params)
+      redirect_to book_path(@book)
+  end
+
+  def destroy
+      @book = Book.find(params[:id])
+      @book.destroy
+      redirect_to books_path
+  end
 
   private
-
   def book_params
-    params.require(:book).permit(:title, :body)
+     params.require(:book).permit(:title, :body)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,9 @@ Rails.application.routes.draw do
   root :to => 'books#top'
   post 'books' => 'books#create'
   get 'books' => 'books#index'
+  get 'books/:id' => 'books#show', as: 'book'
+  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
+  patch 'books/:id' => 'books#update', as: 'update_book'
+  put 'books/:id' => 'books#update'
+  delete 'books/:id' => 'books#destroy'
 end
