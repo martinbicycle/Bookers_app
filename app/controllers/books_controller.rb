71294e6b489d67fd8edd0c330130f96131d3f724
martinbicycle <martinbicycle7700@gmail.com>
2020-12-05 05:14:55 +0000

    [Add] 3-8 投稿画面と一覧画面を同じページに変更

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,14 +2,19 @@ class BooksController < ApplicationController
   def new
     @book = Book.new
   end
-  
+
    def create
     # １. データを新規登録するためのインスタンス作成
     book = Book.new(book_params)
     # ２. データをデータベースに保存するためのsaveメソッド実行
     book.save
-    # ３. トップ画面へリダイレクト
-    redirect_to '/tops'
+
+    redirect_to '/books'
+   end
+
+   def index
+     @books = Book.all
+     @book = Book.new
    end
 
   private
