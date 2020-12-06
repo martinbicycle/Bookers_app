590c0c1752daecbbbae604bcdac290b55e9f05ae
martinbicycle <martinbicycle7700@gmail.com>
2020-12-06 07:19:59 +0000

    [Finish] 3-8 bookers_app

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,11 +5,13 @@ class BooksController < ApplicationController
   end
 
   def create
+
       @book = Book.new(book_params)
       if @book.save
          redirect_to book_path(@book), notice: 'Book was successfully created.'
       else
-         render :new
+         @books = Book.all
+         render :index
       end
   end
 
